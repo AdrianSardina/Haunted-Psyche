9cee17ccb0c97b651c4e3066a921abb14b20d216
AnaCatalina <93290449+AnaCatalina@users.noreply.github.com>
2024-09-28 20:52:54 -0300

    Agregado de Menus e Interfaces

---
+++ Haunted psyche/Assets/Script Menu-Interface/CodigoPantallaCompleta.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CodigoPantallaCompleta : MonoBehaviour
+{
+    public Toggle toggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Screen.fullScreen)
+        {
+            toggle.isOn = true;
+        }
+        else
+        {
+            toggle.isOn = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ActivarPantallaCompleta(bool pantallaCompleta)
+    {
+        Screen.fullScreen = pantallaCompleta;
+    }
+}
